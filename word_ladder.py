fdb1003372f708680576c18c5f756543858f2a64
Fallyn Buckner <Fallyn.Buckner.26@cmc.edu>
2023-02-20 08:59:42 -0800

    first final push

--- word_ladder.py
+++ word_ladder.py
@@ -1,4 +1,6 @@
 #!/bin/python3
+import copy
+from collections import deque
 
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
@@ -23,6 +25,7 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     so you may also get the output
     ```
     ['stone', 'shone', 'shote', 'shots', 'soots',
+    non
     'hoots', 'hooty', 'hooey', 'honey', 'money']
     ```
     (We cannot use doctests here because the outputs are not unique.)
@@ -30,6 +33,30 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     Whenever it is impossible to generate a word ladder between the two words,
     the function returns `None`.
     '''
+    x = True
+    stack = []
+    stack.append(start_word)
+
+    dequei = deque()
+    dequei.append(stack)
+    if start_word == end_word:
+        return stack
+    with open(dictionary_file, 'r') as f:
+        dictionary_file = [word.strip() for word in f]
+
+    while len(dequei) != 0:
+        current = dequei.popleft()
+        copy_dictionary = copy.copy(dictionary_file)
+        for word in copy_dictionary:
+            if _adjacent(word, current[-1]) is x:
+                if word == end_word:
+                    current.append(word)
+                    return current
+                else:
+                    sc = copy.copy(current)
+                    sc.append(word)
+                    dequei.append(sc)
+                    dictionary_file.remove(word)
 
 
 def verify_word_ladder(ladder):
@@ -42,8 +69,12 @@ def verify_word_ladder(ladder):
     >>> verify_word_ladder(['stone', 'shone', 'phony'])
     False
     '''
+
     count = 0
     x = True
+
+    if ladder == []:
+        return False
 
     for i in range(len(ladder) - 1):
         word1 = ladder[i]
@@ -70,12 +101,13 @@ def _adjacent(word1, word2):
     '''
 
     count = 0
-
-    for i in range(len(word1)):
-        if word1[i] in word2:
-            count += 1
-
-    if count == len(word1) - 1:
-        return True
+    if len(word1) == len(word2):
+        for i in range(len(word1)):
+            if word1[i] != word2[i]:
+                count += 1
+        if count > 1:
+            return False
+        else:
+            return True
     else:
         return False
